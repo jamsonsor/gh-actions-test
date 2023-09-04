68f20957a3523e2301d5fd89d2c4104daaadc48a
jamsonsor <jamsons93@gmail.com>
2023-09-04 19:22:52 -0400

    updated test app and added gitignore file

--- test/test.js
+++ test/test.js
@@ -1,7 +1,14 @@
 var request = require('supertest');
 var app = require('../index.js');
+
 describe('GET /will', function() {
     it('respond with hello world', function(done) {
         request(app).get('/will').expect('{ "response": "Hello World" }', done);
     });
+
+    it('respond with a 404 status for a non-existing route', function(done) {
+        request(app).get('/nonexistent').expect(404, done);
+    });
+
+    // Add more test cases as needed
 });
